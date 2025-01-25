cf0c7dece9449a7d25d4146369ad54fb871f4037
jungsikJeong <wndtlr1024@gmail.com>
2025-01-25 18:45:35 +0900

    refactor: 로그인 상태에 따른 컴포넌트 리턴 변경

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -1,23 +1,16 @@
 'use client'
 
 import Link from 'next/link'
-import { Button } from '../ui/button'
-import { useAuth } from '@/hook/useAuth'
-import { createClient } from '@/lib/supabase/client'
+import LoginBtn from '../auth/LoginButton'
+import AuthStatus from '../auth/AuthStatus'
 
 export function Header() {
-  const supabase = createClient()
-
-  const { data: me, error, isPending } = useAuth(supabase)
-  console.log('me:', me)
   return (
     <header className='flex w-full items-center justify-between border-b px-4 py-3 shadow-sm sm:px-6 sm:py-4'>
       <Link href='/' className='text-lg font-bold sm:text-xl'>
         로고
       </Link>
-      <Button size='sm' className='text-sm sm:px-4 sm:text-base'>
-        <Link href='/auth/login'>로그인</Link>
-      </Button>
+      <AuthStatus />
     </header>
   )
 }

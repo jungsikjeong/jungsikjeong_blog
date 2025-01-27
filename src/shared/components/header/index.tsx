9fa3af9ec1ebcb3a418366d2ecba45b2bce64b33
jungsikJeong <wndtlr1024@gmail.com>
2025-01-28 02:00:10 +0900

    styles: header border-b 추가

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -9,8 +9,7 @@ export function Header() {
   const { user } = useAuth()
 
   return (
-    // <header className='h-[65px] bg-neutral-800 px-4 py-2 '>
-    <header className='bg-header h-[65px] px-4 py-2'>
+    <header className='bg-header h-[65px] border-b px-4 py-2'>
       {!user ? <UnauthenticatedHeader /> : <AuthenticatedHeader user={user} />}
     </header>
   )

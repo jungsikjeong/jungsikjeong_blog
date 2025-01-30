53866554c5910f52c70d6a7367bc947b59214f9c
jungsikJeong <wndtlr1024@gmail.com>
2025-01-30 15:58:05 +0900

    styles: readme 섹션 편집 버튼

--- src/features/main/components/Readme/index.tsx
+++ src/features/main/components/Readme/index.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import { useGetProfile } from '@/services/profile/useProfile'
+import { EditActionBtn } from '@/shared/components/buttons'
 
 export default function Readme() {
   const { data: profile } = useGetProfile()
 
   return (
-    <div className='h-[390px] w-full rounded-lg border px-6 py-5'>
+    <div className='relative h-[390px] w-full rounded-lg border px-6 py-5'>
+      <EditActionBtn className='absolute right-4 top-3' variant='ghost' />
+
       <div className='text-sm'>{profile.username} / README.md</div>
     </div>
   )
--- src/shared/components/buttons/EditActionBtn.tsx
+++ src/shared/components/buttons/EditActionBtn.tsx
@@ -4,9 +4,15 @@ import { PencilLine } from 'lucide-react'
 import { Button } from '../ui/button'
 import { cn } from '@/lib/utils'
 
-export default function EditActionBtn({ className }: { className: string }) {
+export default function EditActionBtn({
+  className,
+  variant,
+}: {
+  className: string
+  variant?: 'ghost' | 'default'
+}) {
   return (
-    <Button className={cn('', className)} size={'icon'}>
+    <Button className={cn('', className)} size={'icon'} variant={variant}>
       <PencilLine className='h-4 w-4' />
     </Button>
   )

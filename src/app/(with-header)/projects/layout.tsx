7bd5412621bce88b00f102caa83ee1806ac6fe4a
JungSik Jeong <jungsik.jeong@lcnine.kr>
2025-02-09 14:58:59 +0900

    refactor: 각 섹션별로 layout에 Profile 기입

---
+++ src/app/(with-header)/projects/layout.tsx
@@ -0,0 +1,32 @@
+import { createClient } from '@/lib/supabase/server'
+import { masterProfileQueryOptions } from '@/services/master_profile/queries'
+import Profile from '@/shared/components/profile'
+import { getDehydratedQueries, Hydrate } from '@/utils/react-query'
+import { getCurrentUser } from '@/utils/supabase/auth'
+import React, { Suspense } from 'react'
+
+export default async function layout({ children }: React.PropsWithChildren) {
+  const supabase = await createClient()
+  const user = await getCurrentUser()
+  const queries = await getDehydratedQueries([
+    masterProfileQueryOptions(supabase).getMasterProfile(),
+  ])
+
+  return (
+    <div>
+      <div className='py-4'>
+        <div className='relative flex flex-col px-4 md:container md:flex-row'>
+          <Hydrate state={{ queries }}>
+            <div className='w-full md:w-[300px]'>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Profile user={user} />
+              </Suspense>
+            </div>
+
+            <div className='ml-0 mt-4 flex-1 md:ml-8'>{children}</div>
+          </Hydrate>
+        </div>
+      </div>
+    </div>
+  )
+}
